3d40bbf60bc7e7633aa5fa1ef61f8669234236bf
Jardeas <jelle.dhgh@gmail.com>
2022-10-16 21:02:13 +0200

    add acces

---
+++ js/modules/access.js
@@ -0,0 +1,47 @@
+import { user } from "../config/save";
+
+let guest = [
+    "register",
+    "404",
+    "gasten"
+];
+let memb = [
+    "register"
+];
+let count;
+// GUEST CHECK
+async function guestCheck() {
+    // als de gebruiker offline of niet bestaand is gaat hij gestuurd worden naar pagina "gasten.html"
+    if (user.status === "offline" || user.status === null) {
+        count = 0; // zet de count op 0
+        for (let x = 0; x < guest.length; x++) {
+            if (window.location.pathname === `/${guest[x]}.html`) { // als de pagina waar de gebruiker bevrindt hetzelfde is als 1vd guest array doet hij count +1
+                count++
+                break;
+            }
+        }
+        if (count === 0) { // als de gebruiker op een pagina bevindt waar hij niet moet zijn stuurt hij hen naar de "gasten.html"
+            location.replace("gasten.html");
+        }
+    }
+}
+
+
+async function membCheck() {
+    // als de gebruiker offline of niet bestaand is gaat hij gestuurd worden naar pagina "gasten.html"
+    if (user.status === "online") {
+        for (let x = 0; x < memb.length; x++) {
+            if (window.location.pathname === `/${memb[x]}.html`) { // als de pagina waar de gebruiker bevrindt hetzelfde is als 1vd guest array doet hij count +1
+                location.replace("./index.html");
+                break;
+
+            }
+        }
+ 
+    }
+}
+
+export function acces(){
+    guestCheck();
+    membCheck();
+}
