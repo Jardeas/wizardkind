01e2863bca149cede8b575269b25cca51f32aef7
Jardeas <jelle.dhgh@gmail.com>
2022-10-16 23:13:02 +0200

    edit signup

--- js/base.js
+++ js/base.js
@@ -3,7 +3,7 @@ import "./modules/template"; // genereren van de template
 import "./modules/meters"; // systeem van de simulatie meters
 
 
-import "./user/user"; // Toevoegen van de user_bundle
+import "./user/user_bundle"; // Toevoegen van de user_bundle
 
 
 
