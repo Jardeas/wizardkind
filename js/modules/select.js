1fb2480333067b258ec7fcd84a275ee072e607e8
Jardeas <jelle.dhgh@gmail.com>
2022-10-16 20:34:34 +0200

    add select.js & signup.js

---
+++ js/modules/select.js
@@ -0,0 +1,3 @@
+// SIGNUP 
+export const bttn_aanmaken = document.querySelector('#js_aanmaak'); // ID selecteren van aanmaak button
+export const bttn_inloggen = document.querySelector("#js_inlog"); // ID selecteren van inlog button
